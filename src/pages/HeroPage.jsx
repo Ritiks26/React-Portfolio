382b0b9c29f232ab13b3029b81a038d8b8a11043
Ritik <teragmail@example.com>
2026-03-28 21:47:24 +0530

    update hero section

--- src/pages/HeroPage.jsx
+++ src/pages/HeroPage.jsx
@@ -3,20 +3,44 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 import { CustomEase } from "gsap/CustomEase";
 import { SplitText } from "gsap/all";
-import glassPicture from "../../src/assets/pics/glass.png";
-import { useRef } from "react";
 import "./HeroPage.css";
+import { useRef } from "react";
 
 gsap.registerPlugin(ScrollTrigger, SplitText, CustomEase);
 
 CustomEase.create("hop", "0.85, 0, 0.15, 1");
 
 export function HeroPage() {
+  const heroContainerRef = useRef();
+
+  useGSAP(() => {
+    // document.fonts.ready.then(() => {
+    //   const heroHeading = SplitText.create(".hero-heading h1", {
+    //     type: "chars, words, lines",
+    //   });
+    // });
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: heroContainerRef.current,
+        start: "top top",
+        pin: true,
+        pinSpacing: false,
+        scrub: true,
+      },
+    });
+
+    tl.to(heroContainerRef.current, {
+      scale: 0.87,
+      filter: "blur(0.5rem)",
+    });
+  }, []);
+
   return (
-    <div className="hero-container">
+    <div className="hero-container" ref={heroContainerRef}>
       <div className="hero-heading">
-        <h1>Modern</h1>
-        <h1>Interfaces</h1>
+        <h1>frontend</h1>
+        <h1>developer</h1>
       </div>
 
       <div className="hero-content-and-resume">
