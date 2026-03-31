120e83a8cf2a98a6f442c51119a78525dc242147
Ritik <teragmail@example.com>
2026-03-31 16:25:28 +0530

    update-hero

--- src/pages/HeroPage.jsx
+++ src/pages/HeroPage.jsx
@@ -64,19 +64,18 @@ export function HeroPage() {
 
   return (
     <div className="hero-container" ref={heroContainerRef}>
+      <p className="hero-text hero-left">modern interfaces</p>
+      <p className="hero-text hero-right">built different</p>
+
       <div className="hero-image">
-        <div className="left-hero-image">
-          <img
-            src="https://images.prismic.io/malvah-v2/aZhSN8FoBIGEgmDl_WorkThumbnail_BitterCreek.jpg?auto=format,compress"
-            alt=""
-          />
-        </div>
-        <div className="right-hero-image">
-          <img
-            src="https://images.prismic.io/malvah-v2/aZhk7sFoBIGEgmU__WorkThumbnail_Boring.jpg?auto=format,compress"
-            alt=""
-          />
-        </div>
+        <img
+          src="https://pro2-bar-s3-cdn-cf6.myportfolio.com/cd64cd667b2f99df533be29a125c1486/068a0bc80d7497247680be22_rw_1920.jpg?h=caf1bae525adcdf6078e05f640f368b5"
+          alt=""
+        />
+        <img
+          src="https://pro2-bar-s3-cdn-cf1.myportfolio.com/cd64cd667b2f99df533be29a125c1486/902aa768d3c155a50a7e5b92_rw_1920.jpg?h=31e85fde8cafd2c30dc003d0aff87d87"
+          alt=""
+        />
       </div>
     </div>
   );
