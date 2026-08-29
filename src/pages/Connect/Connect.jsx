8cf737f0a0d2415768291223ad09d0244c6309d2
Ritik <teragmail@example.com>
2026-08-29 20:48:18 +0530

    feature: update/title

--- src/pages/Connect/Connect.jsx
+++ src/pages/Connect/Connect.jsx
@@ -141,64 +141,68 @@ export function Connect({ connectOpen, setConnectOpen }) {
   }, [connectOpen]);
 
   return (
-    <div className="connect-wrapper">
-      <section className="connect-container">
-        <div className="connect-top">
-          <p>CONTACT US</p>
-          <p className="studio-name">RITIK SINGH</p>
-          <div className="connect-address">
-            <p>NEW DELHI</p>
-            <p>INDIA</p>
-            <div className="social-links">
-              <a
-                href="https://www.linkedin.com/in/ritik-singh-899977318/"
-                target="_blank"
-              >
-                LINKEDIN
-              </a>
-              <a href="https://www.facebook.com/RitikSingh2608" target="_blank">
-                FACEBOOK
-              </a>
+    <>
+      <div className="connect-wrapper">
+        <section className="connect-container">
+          <div className="connect-top">
+            <p>CONTACT US</p>
+            <p className="studio-name">RITIK SINGH</p>
+            <div className="connect-address">
+              <p>NEW DELHI</p>
+              <p>INDIA</p>
+              <div className="social-links">
+                <a
+                  href="https://www.linkedin.com/in/ritik-singh-899977318/"
+                  target="_blank"
+                >
+                  LINKEDIN
+                </a>
+                <a
+                  href="https://www.facebook.com/RitikSingh2608"
+                  target="_blank"
+                >
+                  FACEBOOK
+                </a>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="connect-middle">
-          <h2>
-            Have a project, an idea,
-            <br />
-            or just want to say hello?
-          </h2>
-        </div>
+          <div className="connect-middle">
+            <h2>
+              Have a project, an idea,
+              <br />
+              or just want to say hello?
+            </h2>
+          </div>
 
-        <div className="line"></div>
+          <div className="line"></div>
 
-        <div className="connect-bottom">
-          <div className="person-card">
-            <h3>RITIK SINGH</h3>
-            <div>
-              <p>FRONTEND DEVELOPER</p>
-              <p>hritikesingh2608@gmail.com</p>
-              <p>+91 99718 73310</p>
+          <div className="connect-bottom">
+            <div className="person-card">
+              <h3>RITIK SINGH</h3>
+              <div>
+                <p>FRONTEND DEVELOPER</p>
+                <p>hritikesingh2608@gmail.com</p>
+                <p>+91 99718 73310</p>
+              </div>
+            </div>
+            <div className="person-card">
+              <h3>AVAILABLE FOR</h3>
+              <div>
+                <p>FREELANCE</p>
+                <p>INTERNSHIPS</p>
+                <p>FULL TIME ROLES</p>
+              </div>
             </div>
           </div>
-          <div className="person-card">
-            <h3>AVAILABLE FOR</h3>
-            <div>
-              <p>FREELANCE</p>
-              <p>INTERNSHIPS</p>
-              <p>FULL TIME ROLES</p>
-            </div>
-          </div>
+        </section>
+        <div
+          className="close-connect"
+          onClick={() => setConnectOpen(!connectOpen)}
+        >
+          close
         </div>
-      </section>
-
-      <div
-        className="close-connect"
-        onClick={() => setConnectOpen(!connectOpen)}
-      >
-        close
       </div>
-    </div>
+    </>
   );
 }
